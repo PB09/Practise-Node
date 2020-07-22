bf9632b511b62419f5927aaaffc2d2f16ca7cce7
Preet Bhatt <bhattpreet92@gmail.com>
2020-07-22 17:22:23 +0530

    Error callback

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var rect = require('./rectangle');
+const rectangle = require('./rectangle');
 
 function solveRect(l,b){
     console.log("\nSolving for rectangle with L: "+l+" & B: "+b);
-    if(l<=0 || b<=0){
-        console.log("L or B should be greater than zero");
-    }
-    else{
-        console.log("Area of Reactangle is: "+ rect.area(l,b));
-        console.log("Perimeter of Reactangle is: "+ rect.perimeter(l,b));
-    }
+    
+    rect(l,b,(err, angle)=>{
+        if (err){
+            console.log("ERROR:",err.message);
+        }
+        else{
+            console.log("Area of Rectangle for L:"+l+"B:"+b+angle.area()+" & Perimeter is: "+angle.perimeter());
+        }
+    });
+    console.log("\nEnd of Program");
 }
 
 solveRect(4,6);
 solveRect(2,9);
 solveRect(0,2);
 solveRect(1.5,1.5);
+solveRect(0,9);
